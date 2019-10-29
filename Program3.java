4cba39b33526274e7c349353f77e9c0c23f8974d
JitmanyuSingh <57011239+JitmanyuSingh@users.noreply.github.com>
2019-10-29 15:45:53 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -1,13 +1,15 @@
-import java.io*;
+import java.io.*;
 import java.util.*;
-class Program3
+class JSProgram3
 {
 public static void main(String args[])throws IOException
-{
-Scanner sc=new Scanner(System.in);
-int n=0;
-float n1=0.0f;
-if(n==n1)System.out.println("equal");
-else System.out.println("not equal");
+	{
+		Scanner sc=new Scanner(System.in);
+		int j=0;
+		float j1=0.0f;
+		if(j==j1)
+			System.out.println("equal");
+		else System.out.println("not equal");
+	}
 }
-}
+
