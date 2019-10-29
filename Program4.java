0a6d5dc95ff323e5fa2c413f686055aca92ac47c
JitmanyuSingh <57011239+JitmanyuSingh@users.noreply.github.com>
2019-10-29 15:50:39 +0530

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
-class Program4
+class JSProgram4
 {
 	public static void main(String args[]) throws IOException
 	{
 		boolean flag=false;
-    if(flag)flag=false;
-    else flag=true;
+		if(flag)
+			flag=false;
+		else 
+			flag=true;
 		System.out.println(flag);
 	}
 }
